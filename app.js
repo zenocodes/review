ce419ef31ed5b79a84059478733443fe54e57ac1
zenocodes <azronebrian@gmail.com>
2022-06-11 13:05:52 +0300

    added log out functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
 import mysql from 'mysql'
+import session from 'express-session'
 
 const app = express()
 
@@ -15,11 +16,32 @@ const connection = mysql.createConnection({
 // set a tempate engine for the app, ejs (embedded javascript)
 app.set('view engine', 'ejs')
 
+// create session middleware
+app.use(session({
+    secret: 'siri',
+    resave: false,
+    saveUninitialized: false
+}))
+
 // source static files from public
 app.use(express.static('public'))
 
 // configuration to access form information
 app.use(express.urlencoded({extended:false}))
+
+// constantly check if user is logged in
+app.use((req, res, next) => {
+    if(req.session.userID === undefined) {
+        res.locals.isLoggedIn = false
+        console.log('user not signed in')
+    } else {
+        res.locals.isLoggedIn = true
+        res.locals.username = req.session.username
+        res.locals.business = req.session.business
+        console.log(`logged in as a ${req.session.user}`)
+    }
+    next()
+})
 
 // home page
 app.get('/', (req, res) => {
@@ -48,7 +70,11 @@ app.post('/login', (req, res) => {
                 // authenticate
                 bcrypt.compare(req.body.password, results[0].password, (error, matches) => {
                     if(matches) {
-                        res.send('user successfully logged in')
+                        req.session.userID = results[0].userID
+                        req.session.username = results[0].fullname.split(' ')[0]
+                        req.session.user = 'reviewer'
+                        // res.send(`${req.session.userID} : ${req.session.username}`)
+                        res.redirect('/')
                     } else {
                         const user = {
                             email: req.body.email,
@@ -245,6 +271,10 @@ app.post('/business/login', (req, res) => {
             if(results.length > 0) {
                 bcrypt.compare(profile.businessPassword, results[0].b_password, (error, matches) => {
                     if(matches) {
+                        req.session.userID = results[0].b_id
+                        req.session.business = results[0].b_name
+                        req.session.username = results[0].b_contact_person.split(' ')[0]
+                        req.session.user = 'business'
                         res.redirect('/business/dashboard')
                     } else {
                         let message = 'Incorrect Password.'
@@ -262,10 +292,25 @@ app.post('/business/login', (req, res) => {
 
 // business dashboard
 app.get('/business/dashboard', (req, res) => {
-    res.render('business-profile')
+
+    if(res.locals.isLoggedIn && req.session.user === 'business') {
+        res.render('business-profile')
+    } else {
+        res.redirect('/business/login')
+    }
+    
 })
 
 /* .business routes */
+
+
+// logout functionality
+app.get('/logout', (req, res) => {
+    // kill session
+    req.session.destroy((error) => {
+        res.redirect('/')
+    })
+})
 
 // return 404 error
 app.get('*', (req, res) => {
